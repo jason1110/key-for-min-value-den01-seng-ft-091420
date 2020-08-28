9ec7b6246e8a51692b6e56ecd35579ea268eb338
Jason Easterly <jason.easterly11@gmail.com>
2020-08-28 19:32:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,14 @@
 
 def key_for_min_value(name_hash)
 
+<<<<<<< HEAD
 lowest_key = name_hash.default
 min_value = Float::INFINITY
 
+=======
+min_value = Float::INFINITY
+lowest_key = name_hash.default  
+>>>>>>> 47ca0f30155d0d4f0b78809fb9b06c6eb2bb2947
   name_hash.each_pair do |key, value|
     if value < min_value
       min_value = value
@@ -19,4 +24,8 @@ lowest_key
 end
 
 
+<<<<<<< HEAD
 #key_for_min_value(hash)
+=======
+key_for_min_value(hash)
+>>>>>>> 47ca0f30155d0d4f0b78809fb9b06c6eb2bb2947
